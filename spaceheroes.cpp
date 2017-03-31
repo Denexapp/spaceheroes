6a41bd91c71ad984e6bdfc2c04f4d72d79a91fd0
denexapp <denis.mukhametov@gmail.com>
2017-04-01 00:55:03 +0500

    Добавьте файлы проекта.

---
+++ spaceheroes.cpp
@@ -0,0 +1,44 @@
+// SDL2_test_march_17.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <SDL.h>
+
+int main(int argc, char ** argv)
+{
+	if (SDL_Init(SDL_INIT_EVERYTHING) == -1)
+	{
+		std::cout << "Failed to initialize SDL: " << SDL_GetError() << std::endl;
+		return -1;
+	}
+
+	int width = 256;
+	int height = 144;
+	SDL_Window* window;
+
+	window = SDL_CreateWindow("hello", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, 0);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create window: " << SDL_GetError();
+		return -1;
+	}
+
+	SDL_Renderer* renderer;
+
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	if (renderer == nullptr)
+	{
+		std::cout << "Failed to create renderer: " << SDL_GetError();
+	}
+
+	SDL_RenderSetLogicalSize(renderer, width, height);
+	SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
+
+	SDL_RenderClear(renderer);
+
+	SDL_Quit();
+
+	return 0;
+}
+
